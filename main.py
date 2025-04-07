ecae2cb37ea1984d92ea001d40f36f1804a2e65f
Ihor Bozhenko <bozhenkogrand@gmail.com>
2025-04-07 19:39:06 +0300

    Updated main.py and added main interface in CLI.

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import json
+import datetime
 
+def main():
+    #initialise task list
+    task_list = []
+    choice = 0
+    while choice != 6:
+        print("*** Task Manager ***")
+        print("1) Add the task")
+        print("2) Check the task status")
+        print("3) Display all tasks")
+        print("4) ")
+        print("5) ")
+        print("6) Quit")
+        choice = int(input())
+
+if __name__ == "__main__":
+    main()
